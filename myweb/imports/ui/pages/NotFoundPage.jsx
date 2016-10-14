900ef10ab165f6984c9c3ab8beb1c3a2737c54c5
anmatika <antti.matikainen@sievo.com>
2016-10-14 15:08:06 +0300

    rename notfoundpage.jsx

---
+++ myweb/imports/ui/pages/NotFoundPage.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+// Task component - represents a single todo item
+export default class NotFoundPage extends Component {
+  render() {
+    return (
+      <div>Page not found.</div>
+    );
+  }
+}
