c312fed075f90ac6e030f8df25fdd1a5cb8a94ba
anmatika <antti.matikainen@sievo.com>
2016-10-14 15:18:39 +0300

    rename to routes.js

--- myweb/client/main.jsx
+++ myweb/client/main.jsx
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import 'bootstrap/scss/bootstrap.scss';
 import 'tether';
 import 'bootstrap';
-import { renderRoutes } from '../imports/startup/client/routes.jsx';
+import { renderRoutes } from '../imports/startup/client/routes.js';
 
 Meteor.startup(() => {
   render(renderRoutes(), document.getElementById('render-target'));
